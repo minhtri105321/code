f539481bae1e68fd5bbd4490324c58c054c32fdb
minhtri105321 <tripm.21it@vku.udn.vn>
2023-04-20 09:09:14 +0700

    baiktra2

--- KTFirebase/KTFirebase/app/src/main/java/com/example/kt_firebase/modules/Employees.kt
+++
@@ -1,9 +0,0 @@
-package com.example.kt_firebase.modules
-
-data class Employees(
-    var empId: String? = null,
-    var empName: String? = null,
-    var empAge: String? = null,
-    var empJob: String? = null,
-    var empBirthday: String? = null
-    )
